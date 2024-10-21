caa041a04b85fa23c6683b514e0d5d3c612d1097
Anuj5504 <anujjadhav0505@gmail.com>
2024-10-21 17:19:35 +0530

    code

---
+++ Portal/src/redux/actions/authAction.js
@@ -0,0 +1,30 @@
+
+
+export const updateUser = (userData) => {
+    return async (dispatch, getState) => {
+        const { accessToken } = getState().auth; // Access the token from Redux state
+        console.log(accessToken)
+        dispatch({ type: 'UPDATE_USER_REQUEST' });
+
+        try {
+            const response = await fetch('http://localhost:9000/api/v2/users/update-account', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify(userData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                dispatch({ type: 'UPDATE_USER_SUCCESS', payload: data });
+            } else {
+                throw new Error(data.message || 'Error updating user');
+            }
+        } catch (error) {
+            dispatch({ type: 'UPDATE_USER_FAILURE', payload: error.message });
+        }
+    };
+};
