be54945d373a6d802737031d288f2d95e2447976
rushi992145 <rushikeshiray99@gmail.com>
2024-10-21 18:07:46 +0530

    USER PUSH

--- Portal/src/redux/store.js
+++ Portal/src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postReducer from './postSlice';
 import authReducer from './authSlice';
 
 const store = configureStore({
   reducer: {
-    post: postReducer,
     auth: authReducer,
   },
 });
